8bbe08b06510481d841cb57c6a8b1428cf927e4b
Маруся <@192.168.0.7>
2018-03-07 00:42:15 +0300

    start

---
+++ Plums.java
@@ -0,0 +1,10 @@
+
+class Plums{
+	public static void main(String args []) {
+		int counter = 0;
+		while (counter < 15) {
+			System.out.println("Plums are better than apples");
+			counter++;
+}
+}
+}
